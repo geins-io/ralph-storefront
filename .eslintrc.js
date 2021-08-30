ca4b1ca0b89096c679ce22b96ef66b3e2ba70bda
Lars Johansson <Johansson@carismar.com>
2021-08-30 15:49:14 +0200

    Should fix husky bug

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,18 +7,17 @@ module.exports = {
   parserOptions: {
     parser: 'babel-eslint'
   },
-  extends: [
-    '@nuxtjs',
-    'prettier',
-    'prettier/vue',
-    'plugin:prettier/recommended',
-    'plugin:nuxt/recommended'
-  ],
-  plugins: ['prettier'],
+  extends: ['@nuxtjs', 'plugin:nuxt/recommended', 'prettier', 'prettier/vue'],
   // add your custom rules here
   rules: {
     'linebreak-style': 0,
     quotes: ['error', 'single'],
     'vue/no-unused-components': 1
-  }
+  },
+  ignorePatterns: [
+    'node_modules',
+    'dist',
+    'antlr',
+    '**/unifaun-checkout-all.min.js'
+  ]
 };
