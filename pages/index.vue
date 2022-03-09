17fd5d36ca1afc444e072a3e1dc0532052de630d
Maveldous <arttime32@gmail.com>
2022-03-09 10:05:14 +0200

    feat(indexPage): add client-only render

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,21 @@
 <template>
   <div class="ca-front-page">
-    <CaWidgetArea
-      family="Frontpage"
-      area-name="The front page area"
-      @dataFetched="$store.dispatch('loading/end')"
-    />
+    <client-only>
+      <CaWidgetArea
+        family="Frontpage"
+        area-name="The front page area"
+        @dataFetched="$store.dispatch('loading/end')"
+      />
+    </client-only>
   </div>
 </template>
 
 <script>
 export default {
   name: 'FrontPage',
+  components: {
+    CaWidgetArea: () => import('~/node_modules/@ralph/ralph-ui/components/molecules/CaWidgetArea/CaWidgetArea')
+  },
   data: () => ({}),
   methods: {}
 };
