f7885c23422413faa30fd94e68188d30aef7ba1c
Maveldous <arttime32@gmail.com>
2022-03-10 13:01:47 +0200

    fix(IndexPage): temporary rm client-only tags

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,16 @@
 <template>
   <div class="ca-front-page">
-    <client-only>
-      <CaWidgetArea
-        family="Frontpage"
-        area-name="The front page area"
-        @dataFetched="$store.dispatch('loading/end')"
-      />
-    </client-only>
+    <CaWidgetArea
+      family="Frontpage"
+      area-name="The front page area"
+      @dataFetched="$store.dispatch('loading/end')"
+    />
   </div>
 </template>
 
 <script>
 export default {
   name: 'FrontPage',
-  components: {
-    CaWidgetArea: () => import('~/node_modules/@ralph/ralph-ui/components/molecules/CaWidgetArea/CaWidgetArea')
-  },
   data: () => ({}),
   methods: {}
 };
