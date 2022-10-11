312935f6c6e94d3fc2a8cfa2b792c9814393e7b9
Martin Drakulovski <40492624+martindrakul@users.noreply.github.com>
2022-10-11 11:38:48 +0200

    update ralph ui and add option for multiple markets

--- nuxt.config.js
+++ nuxt.config.js
@@ -472,7 +472,7 @@ export default async () => {
         defaultPaymentId: 23,
         defaultShippingId: null
       },
-      showMultipleMarkets: true,
+      showMultipleMarkets: false,
       /* ******************** */
       /* ******* CART ******* */
       /* ******************** */
