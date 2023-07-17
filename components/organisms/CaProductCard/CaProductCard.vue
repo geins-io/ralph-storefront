82217bb7a7698f7fbea3521d3f613a61433d221d
Olivia Axelsson <axelsson@geins.io>
2023-07-17 17:21:59 +0200

    chore: remove use of deprecated images

--- components/organisms/CaProductCard/CaProductCard.vue
+++ components/organisms/CaProductCard/CaProductCard.vue
@@ -9,7 +9,7 @@
         @click.native="productClickHandler"
       >
         <CaImage
-          v-if="product.images !== null && product.images.length > 0"
+          v-if="product.productImages && product.productImages.length > 0"
           class="ca-product-card__image"
           type="product"
           :size-array="
@@ -20,7 +20,7 @@
             )
           "
           :ratio="$config.productImageRatio"
-          :filename="product.images[0]"
+          :filename="product.productImages[0].fileName"
           :alt="product.brand.name + ' ' + product.name"
           sizes="(min-width: 1360px) 248px, (min-width: 1024px) 18.23vw, (min-width: 768px) 30.73vw, 48vw"
         />
